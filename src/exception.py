e55c070311152b61877e672c067c8beb82e09f5b
LAPTOP-51AC8SD6\hlakr <hlakra2000@gmail.com>
2025-07-10 18:23:59 +0530

    Modified exception file

--- src/exception.py
+++ src/exception.py
@@ -8,7 +8,7 @@ def error_message_detail(error,error_detail:sys):
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
         file_name, exc_tb.tb_lineno,str(error))
     
-
+    return error_message
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
@@ -21,7 +21,11 @@ class CustomException(Exception):
 
 if __name__=="__main__":
     try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by zero")
-        raise CustomException(e,sys)
+        logging.info("Trying to open a file that doesn't exist.")
+        with open("non_existing_file.txt", "r") as f:
+            f.read()
+
+    except FileNotFoundError as e:
+        logging.error("Caught a FileNotFoundError")
+        raise CustomException(e, sys)
+    
